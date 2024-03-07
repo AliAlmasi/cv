efde7baf7c9cb1e94e2d312c8dfb6fbc4c50c999
Ali Almasi <A710almasi@gmail.com>
2024-03-07 04:02:33 +0330

    Major optimizations in styles and scripts ~ TODO: make it responsive!

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 "use strict";
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-const doctitle = document.title;
-const titleReset = async () => {
-	await sleep(100);
-	document.title = doctitle;
-};
-Mousetrap.bind(["command+p", "ctrl+p"], (e) => {
-	e.preventDefault();
-	document.title = "AliAlmasi";
-	window.print();
-	titleReset();
-});
-// document.addEventListener("click", (e) => {
-// 	e.preventDefault();
-// 	document.title = "AliAlmasi";
-// 	window.print();
-// 	titleReset();
-// });
+const mainTitle = document.title;
+window.addEventListener("beforeprint", () => (document.title = "AliAlmasi"));
+window.addEventListener("afterprint", () => (document.title = mainTitle));
