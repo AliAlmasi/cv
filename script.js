3df17f020c185340953853e181bdfa0e3886ef97
Ali Almasi <a710almasi@gmail.com>
2024-07-10 02:09:02 +0330

    added my projects + changed second level bullets type + js optimizations todo: modal for job details & minify

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 "use strict";
+window.addEventListener("load", () => {
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth"
+	});
+});
+
+document
+	.querySelector("#print")
+	.addEventListener("click", () => window.print());
+
 const mainTitle = document.title;
 window.addEventListener("beforeprint", () => (document.title = "AliAlmasi"));
 window.addEventListener("afterprint", () => (document.title = mainTitle));
