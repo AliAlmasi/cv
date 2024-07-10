9596a68620a5e814ff21c94fa42dbf4eba426335
Ali Almasi <a710almasi@gmail.com>
2024-07-10 16:20:40 +0330

    changed page bg color + css optimization + removed js load listener + added fallback fonts

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 "use strict";
-window.addEventListener("load", () => {
-	window.scrollTo({
-		top: 0,
-		left: 0,
-		behavior: "smooth"
-	});
-});
-
 document
 	.querySelector("#print")
 	.addEventListener("click", () => window.print());
