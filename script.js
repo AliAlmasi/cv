d32ebed3ca0c9525cc7990c3611724ff1c0a0fb7
Ali Almasi <a710almasi@gmail.com>
2024-08-12 02:04:07 +0330

    slight changes

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 "use strict";
-document
-	.querySelector("#print")
-	.addEventListener("click", () => window.print());
-
 const mainTitle = document.title;
-window.addEventListener("beforeprint", () => (document.title = "AliAlmasi"));
-window.addEventListener("afterprint", () => (document.title = mainTitle));
+const printFileName = "AliAlmasi";
+
+document
+	.querySelectorAll("a")
+	.forEach((tag) => (tag.href = tag.href.toString().toLowerCase()));
+
+function checkFlexGap() {
+	var flex = document.createElement("div");
+	flex.style.display = "flex";
+	flex.style.flexDirection = "column";
+	flex.style.rowGap = "1px";
+
+	flex.appendChild(document.createElement("div"));
+	flex.appendChild(document.createElement("div"));
+
+	document.body.appendChild(flex);
+	var isSupported = flex.scrollHeight === 1;
+	flex.parentNode.removeChild(flex);
+
+	if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+window.addEventListener("beforeprint", () => {
+	document.title = printFileName;
+	document
+		.querySelectorAll("div.upper-text__infobox a")
+		.forEach((tag) => (tag.style.color = "var(--secondary)"));
+});
+
+window.addEventListener("afterprint", () => {
+	document.title = mainTitle;
+	document
+		.querySelectorAll("div.upper-text__infobox a")
+		.forEach((tag) => (tag.style.color = "unset"));
+});
+
+document.addEventListener("keydown", (e) => {
+	if ((e.key == "p" || e.key == "P") && e.ctrlKey) {
+		e.preventDefault();
+		document.querySelector("#print").click();
+	}
+});
